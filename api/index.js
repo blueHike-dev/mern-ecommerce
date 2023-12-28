72719708902cf6236b8e170c7d04088c14df5f7e
blueHike <wambuisimon712@gmail.com>
2023-12-28 15:03:51 +0300

    first commit

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
+// const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
+const cors = require('cors')
 
 mongoose
 .connect(process.env.MONGO_URI)
@@ -11,7 +14,12 @@ mongoose
   console.log(err)
 })
 
+app.use(cors())
+app.use(express.json());
+app.use('/api/auth', authRoute)
+// app.use('/api/users', userRoute)
 
-app.listen(process.env.PORT || 5000, () => {
+
+app.listen(process.env.PORT || 3000, () => {
   console.log('Backend Server is running!');
 })
