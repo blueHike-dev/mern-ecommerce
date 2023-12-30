e4f427b8fae7104a6a42ef118b827c7de12af5ba
blueHike <wambuisimon712@gmail.com>
2023-12-30 21:38:37 +0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,6 +39,24 @@ const App = () => {
           quaerat atque. Iusto, voluptatem ipsum?
         </h2>
       </div>
+      <div className="div">
+        <h1 className="one">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
+          beatae enim necessitatibus ad veritatis incidunt quas fugit nesciunt,
+          odit, magni illum, illo aspernatur exercitationem tempore itaque id.
+          Laborum eius commodi doloremque culpa totam deleniti modi quas, nobis
+          asperiores dolorem pariatur dolores unde fuga est eos doloribus
+          mollitia quia magnam debitis.
+        </h1>
+        <h2 className="two">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quae
+          repudiandae incidunt quisquam, vero inventore porro? Qui ratione quam
+          amet recusandae quo aliquam consequatur distinctio officiis sit,
+          consectetur modi. Ipsam velit quod id, sunt, possimus reprehenderit
+          nobis qui veniam, unde impedit deserunt optio necessitatibus soluta
+          quaerat atque. Iusto, voluptatem ipsum?
+        </h2>
+      </div>
     </>
   );
 };
