ed99f44fa10062791bad05a29c5abd77553aefe8
blueHike <wambuisimon712@gmail.com>
2023-12-30 10:24:57 +0300

    first commit

--- api/index.js
+++ api/index.js
@@ -3,8 +3,11 @@ const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
-// const userRoute = require('./routes/user')
+const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
+const productRoute = require('./routes/product')
 const cors = require('cors')
 
 mongoose
@@ -17,7 +20,10 @@ mongoose
 app.use(cors())
 app.use(express.json());
 app.use('/api/auth', authRoute)
-// app.use('/api/users', userRoute)
+app.use('/api/users', userRoute)
+app.use('/api/carts', cartRoute)
+app.use('/api/orders', orderRoute)
+app.use('/api/products', productRoute)
 
 
 app.listen(process.env.PORT || 3000, () => {
