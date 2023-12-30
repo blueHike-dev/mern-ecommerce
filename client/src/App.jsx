4cdca1ac7812fe84273e0da402bfb3b6bc9eae7a
blueHike <wambuisimon712@gmail.com>
2023-12-30 21:38:03 +0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <div>
+        <h1 className="one">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
+          beatae enim necessitatibus ad veritatis incidunt quas fugit nesciunt,
+          odit, magni illum, illo aspernatur exercitationem tempore itaque id.
+          Laborum eius commodi doloremque culpa totam deleniti modi quas, nobis
+          asperiores dolorem pariatur dolores unde fuga est eos doloribus
+          mollitia quia magnam debitis.
+        </h1>
+        <h2 className="two">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quae
+          repudiandae incidunt quisquam, vero inventore porro? Qui ratione quam
+          amet recusandae quo aliquam consequatur distinctio officiis sit,
+          consectetur modi. Ipsam velit quod id, sunt, possimus reprehenderit
+          nobis qui veniam, unde impedit deserunt optio necessitatibus soluta
+          quaerat atque. Iusto, voluptatem ipsum?
+        </h2>
+      </div>
+      <div className="div">
+        <h1 className="one">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
+          beatae enim necessitatibus ad veritatis incidunt quas fugit nesciunt,
+          odit, magni illum, illo aspernatur exercitationem tempore itaque id.
+          Laborum eius commodi doloremque culpa totam deleniti modi quas, nobis
+          asperiores dolorem pariatur dolores unde fuga est eos doloribus
+          mollitia quia magnam debitis.
+        </h1>
+        <h2 className="two">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quae
+          repudiandae incidunt quisquam, vero inventore porro? Qui ratione quam
+          amet recusandae quo aliquam consequatur distinctio officiis sit,
+          consectetur modi. Ipsam velit quod id, sunt, possimus reprehenderit
+          nobis qui veniam, unde impedit deserunt optio necessitatibus soluta
+          quaerat atque. Iusto, voluptatem ipsum?
+        </h2>
+      </div>
+    </>
+  );
 };
 
 export default App;
