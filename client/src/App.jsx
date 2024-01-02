b8b25688625e54567ea64f4cb1651580efb83b29
blueHike <wambuisimon712@gmail.com>
2024-01-02 22:15:03 +0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div>hello world!</div>;
+  return;
+  <>
+    <Router>
+      <Header />
+      <Main />
+      <Footer />
+    </Router>
+  </>;
 };
 
 export default App;
