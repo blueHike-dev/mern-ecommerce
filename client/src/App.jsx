3463b79edf51861066083b340e1101d7ecb35cd6
blueHike <wambuisimon712@gmail.com>
2024-01-04 12:18:51 +0300

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,58 +1,40 @@
 import React from "react";
 import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-  Navigate,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Redirect,
 } from "react-router-dom";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import { useSelector } from "react-redux";
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Success from "./pages/Success";
 
 const App = () => {
-  const user = useSelector((state) => state.user.currentUser);
+  // const user = useSelector((state) => state.user.currentUser);
 
-  const Layout = () => {
-    <div>
-      <Header />
-      <div>
-        <Outlet />
-      </div>
-      <Footer />
-    </div>;
-  };
-
-  const ProtectedRoute = ({ children }) => {
-    if (!user) {
-      return <Navigate to="/login" />;
-    }
-
-    return children;
-  };
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <ProtectedRoute>
-          <Layout />
-        </ProtectedRoute>
-      ),
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/cart",
-        },
-      ],
-    },
-  ]);
-
-  return;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="success">
+          <Success />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
