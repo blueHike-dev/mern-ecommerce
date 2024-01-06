b6ad8ce447356fa6a20799f1c9f06025d8b9b5ad
simo blues <wambuisimon712@gmail.com>
2024-01-06 11:50:03 +0300

    changes

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,4 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+
+const data = [
+  {
+    id: 1,
+    title: "always fresh & always crispy & always hot",
+    img:"https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+  },
+  {
+    id: 2,
+    title: "we deliver your order wherever you are in NY",
+    img:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+  },
+  {
+    id: 3,
+    title: "the best pizza to share with your family",
+    img:"https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+  },
+];
 
 const Slider = () => {
 
@@ -13,7 +32,17 @@ const Slider = () => {
       return () => clearInterval(interval);
     }, []);
   return (
-    <div>Slider</div>
+   <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
+    <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+      <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
+        {data[currentSlide].title}
+      </h1>
+      <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
+    </div>
+    <div className="w-full flex-1 relative">
+      <img src={data[currentSlide].img} alt="" className='object-cover'/>
+    </div>
+   </div>
   )
 }
 
--- client/src/data.js
+++ client/src/data.js
@@ -4,17 +4,17 @@ export const sliderItems = [
     title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
     desc:"Your perfect pack for everyday use and walks in the forest",
     bg : "f5fafd",
-    image:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"},
+    img:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"},
     {
     id:2,
     title:"Mens Casual Premium Slim Fit T-Shirts ",
     desc:"Slim-fitting style, contrast raglan long sleeve, three-button henley placket",
     bg : "fcf1ed",
-    image:"https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"},
+    img:"https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"},
     {
     id:3,
     title:"Mens Cotton Jacket",
     desc:"great outerwear jackets for Spring/Autumn/Winter",
     bg : "fbf0f4",
-    image:"https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"},
+    img:"https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"},
 ]
